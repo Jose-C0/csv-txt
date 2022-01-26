60ee85300e82e098a7cff20d8f5d67900dc3d794
Jose Francisco <rocj1209@gmail.com>
2022-01-26 03:30:35 -0400

    Primer commit

---
+++ Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csv_txt
+{
+    public class Product
+    {
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public double Precio { get; set; }
+        public string Categoriaa { get; set; }
+
+    }
+}
