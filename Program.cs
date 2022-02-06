d2bc52614f0863da3bb891d0f9120ae560183369
Jose <92603352+RODCJ1@users.noreply.github.com>
2022-02-06 18:21:41 -0400

    Update Program

--- Program.cs
+++ Program.cs
@@ -10,83 +10,102 @@ namespace csv_txt
         static void Main(string[] args)
         {
             #region csv
-            //********************** CASO 1: METODO PARA CREAR UN DIRECTORIO ********************** 
+           //********************** CASO 1: METODO PARA CREAR UN DIRECTORIO ********************** 
+            Stream archivo;
             string dir = @"C:\Ruta\DirectorioCSV";
             if (!Directory.Exists(dir))
                 Directory.CreateDirectory(dir);
-            
-            //********************** CASO 2: Borrar archivo o crear  ********************** 
-            String path = dir + "\\Products.txt";
-            if (!File.Exists(path))
-                File.Delete(path);
+
+            //********************** CASO 2: Crear archivo    ********************** 
+             string path = dir + @"\Products.csv";
+
+            if (!File.Exists(path)) //Para crear el archivo donde se almacena la información   de los pacientes filtrado por grupo sanguineo
+            {
+                archivo = File.Create(path);
+                archivo.Close();
+            }
+ 
+
+
+            //********************** CASO 4: ESCRIBIR CSV Sobreescribiendo ********************** 
+            StreamWriter csvOut =
+                new StreamWriter(
+                    new FileStream(path, FileMode.Append, FileAccess.Write));
+
+            List<Product> listEnVs = new List<Product>();
+
+            listEnVs.AddRange(
+                new Product[]
+                {
+                    new Product { Nombre = "Pistola", Precio = 2323.41, Cantidad = 3,  Categoriaa = "Componentes diversos " },
+                    new Product { Nombre = "Lubricante", Precio = 22222.65, Cantidad = 73, Categoriaa = "Componentes diversos " },
+                    new Product { Nombre = "Amortiguadores", Precio = 1330000, Cantidad = 22, Categoriaa = "Componentes diversos " },
+                    new Product { Nombre = "Boquillas", Precio = 100040, Cantidad = 10, Categoriaa = "Componentes diversos " },
+                    new Product { Nombre = "Casquillos", Precio = 32322.65, Cantidad = 4, Categoriaa = "Componentes diversos " },
+                    new Product { Nombre = "Escalera Mediana", Precio = 99.99, Cantidad = 200, Categoriaa = "Escalera" },
+                    new Product { Nombre = "Escalera Grande", Precio = 80.24, Cantidad = 2, Categoriaa = "Escalera" },
+                    new Product { Nombre = "Esmaltes", Precio = 544.60, Cantidad = 100, Categoriaa = "Pinturas" },
+                    new Product { Nombre = "Pintura Azul", Precio = 33.22, Cantidad = 30, Categoriaa = "Pinturas" },
+                    new Product { Nombre = "Pintura Roja", Precio = 55.99 , Cantidad = 53, Categoriaa = "Pinturas" },
+                    new Product { Nombre = "Pintura Negra", Precio = 100.99, Cantidad = 999999, Categoriaa = "Pinturas" }
+                });
+
+
+
+            foreach (Product x in listEnVs)
+            {
+                csvOut.Write(x.Nombre + ",");
+                csvOut.Write(x.Cantidad + ",");
+                csvOut.Write(x.Precio + ",");
+                csvOut.WriteLine(x.Categoriaa);
+            }
+
+            csvOut.Close();
 
             //********************** CASO 3: LEER CSV ********************** 
             StreamReader csvIn =
                 new StreamReader(
-                    new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read));
+                    new FileStream(path, FileMode.Open, FileAccess.Read));
 
             List<Product> products = new List<Product>();
             while (csvIn.Peek() != -1)
             {
-                string row = csvIn.ReadLine();
+                string row = csvIn.ReadToEnd();
                 string[] columns = row.Split(',');
 
-                Product product = new Product();
-                product.Nombre = columns[0];
-                product.Cantidad = Convert.ToInt32(columns[1]);
-                product.Precio = Convert.ToDouble(columns[2]);
-                product.Categoriaa = columns[3];
+
+                //Product product = new Product();
+                //product.Nombre = columns[0];
+                //product.Cantidad = Convert.ToInt32(columns[1]);
+                //product.Precio = Convert.ToDouble(columns[2]);
+                //product.Categoriaa = columns[3];
+
+                Product product = new Product()
+                {
+                    Nombre = columns[0],
+                    Cantidad = Convert.ToInt32(columns[1]),
+                    Precio = Convert.ToDouble(columns[2]),
+                    Categoriaa = columns[3]
+
+                };
+
                 products.Add(product);
+
             }
             csvIn.Close();
 
             products.ForEach(x => Console.WriteLine($"{x.Nombre}, {x.Cantidad}, {x.Precio}, {x.Categoriaa}"));
 
 
-            //********************** CASO 4: ESCRIBIR CSV Sobreescribiendo ********************** 
-            StreamWriter csvOut =
-                new StreamWriter(
-                    new FileStream(path, FileMode.Create, FileAccess.Write));
-
-            List<Product> listEnVs = new List<Product>();
-
-            listEnVs.AddRange(
-                new Product[]
-                {
-                    new Product { Nombre = "Pistola", Precio = 2323.41, Cantidad = 3,  Categoriaa = "Componentes diversos " },
-                    new Product { Nombre = "Lubricante", Precio = 22222.65, Cantidad = 73, Categoriaa = "Componentes diversos " },
-                    new Product { Nombre = "Amortiguadores", Precio = 1330000, Cantidad = 22, Categoriaa = "Componentes diversos " },
-                    new Product { Nombre = "Boquillas", Precio = 100040, Cantidad = 10, Categoriaa = "Componentes diversos " },
-                    new Product { Nombre = "Casquillos", Precio = 32322.65, Cantidad = 4, Categoriaa = "Componentes diversos " },
-                    new Product { Nombre = "Escalera Mediana", Precio = 99.99, Cantidad = 200, Categoriaa = "Escalera" },
-                    new Product { Nombre = "Escalera Grande", Precio = 80.24, Cantidad = 2, Categoriaa = "Escalera" },
-                    new Product { Nombre = "Esmaltes", Precio = 544.60, Cantidad = 100, Categoriaa = "Pinturas" },
-                    new Product { Nombre = "Pintura Azul", Precio = 33.22, Cantidad = 30, Categoriaa = "Pinturas" },
-                    new Product { Nombre = "Pintura Roja", Precio = 55.99 , Cantidad = 53, Categoriaa = "Pinturas" },
-                    new Product { Nombre = "Pintura Negra", Precio = 100.99, Cantidad = 999999, Categoriaa = "Pinturas" }
-                });
-
-
-
-            foreach (Product x in listEnVs)
-            {
-                csvOut.Write(x.Nombre + ",");
-                csvOut.Write(x.Cantidad + ",");
-                csvOut.Write(x.Precio + ",");
-                csvOut.WriteLine(x.Categoriaa);
-            }
-
-            csvOut.Close();
-
 
             //********************** CASO 5: ESCRIBIR CSV CON APPEND ********************** 
-            StreamWriter csvOut =
+            StreamWriter csvOut2 =
                 new StreamWriter(
                     new FileStream(path, FileMode.Append, FileAccess.Write));
 
 
 
-            List<Product> listEnVs = new List<Product>();
+           listEnVs = new List<Product>(); 
 
             listEnVs.AddRange(
                 new Product[]
@@ -104,20 +123,17 @@ namespace csv_txt
                     new Product { Nombre = "Pintura Negra", Precio = 100.99, Cantidad = 999999, Categoriaa = "Pinturas" }
                 });
 
-            csvOut.WriteLine();
+            csvOut2.WriteLine();
 
             foreach (Product x in listEnVs)
             {
-                csvOut.Write(x.Nombre + ",");
-                csvOut.Write(x.Cantidad + ",");
-                csvOut.Write(x.Precio + ",");
-                csvOut.WriteLine(x.Categoriaa);
+                csvOut2.Write(x.Nombre + ",");
+                csvOut2.Write(x.Cantidad + ",");
+                csvOut2.Write(x.Precio + ",");
+                csvOut2.WriteLine(x.Categoriaa);
             }
 
-            csvOut.Close();
-
-
-
+            csvOut2.Close();
 
             #endregion
 
